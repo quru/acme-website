815ea08a8fdd0717d70e6a7ec1e66b8ae277c7c5
James Freeman <james@quru.com>
2017-10-26 00:30:20 +0100

    Added nohup to startup script, and added code to exit with an error if we try and start on top of an already running web server.

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ ! -f /var/run/http_server.pid ]; then
-    python -m SimpleHTTPServer 80 >> access.log 2>&1 &
+    nohup python -m SimpleHTTPServer 80 >> access.log 2>&1 &
     echo $! > /var/run/http_server.pid
+else
+    echo "Web server already running - please stop it first."
+    exit 1
 fi
