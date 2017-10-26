4f35420bf83bd266e00464363fe403c7078239e7
James Freeman <james@quru.com>
2017-10-26 02:09:57 +0100

    Added code to start_server.sh to fail on server already running

--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,7 @@
 if [ ! -f /var/run/http_server.pid ]; then
     nohup python -m SimpleHTTPServer 80 >> access.log 2>&1 &
     echo $! > /var/run/http_server.pid
+else
+    echo "Web server already running - please stop it first."
+    exit 1
 fi
