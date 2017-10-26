3f2cc5d392db365e552555a5dcfb5400ad3a083f
James Freeman <root@ansible-client2.qurulabs>
2017-10-26 05:34:04 +0100

    Added a deliberately broken startup script for testing purposes

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f /var/run/http_server.pid ]; then
-    nohup python -m SimpleHTTPServer 80 >> access.log 2>&1 &
+    nohup notpython -m SimpleHTTPServer 80 >> access.log 2>&1 &
     echo $! > /var/run/http_server.pid
 else
     echo "Web server already running - please stop it first."
